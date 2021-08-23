372ae45d293bfb33510ab300f95025fd9d17b290
ashar73 <83685910+ashar73@users.noreply.github.com>
2021-08-23 14:24:24 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,5 +2,6 @@ class Simple{
   public static void main(String args[]) throws InterruptedException{
     Thread.sleep(10000);
     System.out.println("Hello Java Updated");
+     System.out.println("Hello Java Updated added to branch b1");
     }
 }
